cb7688fe5dacea932cff6a78779b80e477b48517
devmain <limadanielpereira@gmail.com>
2024-05-31 04:36:52 -0300

    Adiciona login parcial

--- controllers/login.py
+++ controllers/login.py
@@ -2,22 +2,6 @@
 from flask import Blueprint, request, render_template, redirect, url_for
 login_ = Blueprint("login_",__name__, template_folder="templates")
 
-users = {
-    'user1':'1234',
-    'user2':'12345'
-}
-@login_.route('/validated_user', methods=['POST'])
-def validated_user():
-    if request.method == 'POST':    
-        user = request.form['user']
-        password = request.form['password']
-        print(user, password)
-        if user in users and users[user] == password:
-            return render_template('base.html')
-        else:
-            return '<h1>invalid credentials!</h1>'
-    else:
-        return render_template('base.html')
     
 @login_.route('/add_user', methods=['GET','POST'])
 def add_user():
